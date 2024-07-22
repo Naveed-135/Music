a3bee2f159b02c9f89d98b27b10eb6f88012376c
Naveed135 <naveedahamad135@gmail.com>
2024-07-22 15:52:43 +0500

    Update page.tsx

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -12,8 +12,8 @@ function page() {
         <div className="flex flex-wrap justify-center">
             {
                 courseData.courses.map((course,index) => (
-          <div key={index}>
-            <CardContainer className="inter-var m-4">
+        
+            <CardContainer key={index} className="inter-var m-4">
             <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
               <CardItem
                 translateZ="50"
@@ -58,7 +58,7 @@ function page() {
                 </CardItem>
               </div>
             </CardBody>
-          </CardContainer></div>
+          </CardContainer>
                 ))
 
             }
